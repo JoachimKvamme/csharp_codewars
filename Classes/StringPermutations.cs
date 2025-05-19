7fc09fa37f6b98c50869edd2fbd60d5823c54c2a
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-05-19 14:55:39 +0200

    SinglePermutation printer ut alle variantene man kan skrive med strengen den får inn, men jeg har ikke funnet ut hvordan man returnerer verdiene

--- Classes/StringPermutations.cs
+++ Classes/StringPermutations.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace csharp_codewars
@@ -8,11 +9,36 @@ namespace csharp_codewars
     public static class StringPermutations
     {
 
-        public static List<string> SinglePermutations(string s)
+        public static void SinglePermutations(string s)
         {
-            var charlist = s.ToList();
+            char[] chars = s.ToArray();
+            SinglePermutations(chars, 0);
+        }
 
-            if (charlist.Count() == 1) return charlist.Select(l => new List<string> {l})
+        private static void SinglePermutations(char[] list, int fi)
+        {
+
+            List<string> result = [];
+
+            if (fi == list.Length - 1)
+            {
+                Console.WriteLine(list);
+            }
+            else
+                for (int i = fi; i < list.Length; i++)
+                {
+                    Swap(ref list[fi], ref list[i]);
+                    SinglePermutations(list, fi + 1);
+                    Swap(ref list[fi], ref list[i]);
+                }
+        }
+
+        private static void Swap(ref char a, ref char b)
+        {
+            if (a == b) return;
+            var temp = a;
+            a = b;
+            b = temp;
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine();
+        StringPermutations.SinglePermutations("abcde");
         
     }
 
