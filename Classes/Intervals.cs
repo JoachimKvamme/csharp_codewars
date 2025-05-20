b88de972ed020e9da326ce705d7213947144ea71
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-05-20 09:27:24 +0200

    startet på sumIntervals

---
+++ Classes/Intervals.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp_codewars.Classes
+{
+    public class Intervals
+    {
+        public static int SumIntervals((int, int)[] intervals)
+        {
+            return -1;
+        }
+    }
+}
