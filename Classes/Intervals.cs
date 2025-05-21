0e01d8eb166429e7a83388c40839c7cb7369fe89
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-05-21 11:18:48 +0200

    Implementerte GeeksForGeeks' "naive" metode for å smelte sammen overlappende intervaller. Nå ser det ut til å virke

--- Classes/Intervals.cs
+++ Classes/Intervals.cs
@@ -9,9 +9,51 @@ namespace csharp_codewars.Classes
     {
         public static int SumIntervals((int, int)[] intervals)
         {
-            return -1;
+
+            int sum = 0;
+
+            List<(int, int)> mergedIntervals = MergeOverlappingIntervals(intervals);
+
+            foreach (var item in mergedIntervals)
+            {
+                sum += item.Item2 - item.Item1;
+            }
+
+            return sum;
         }
 
-        
+        public static List<(int, int)> MergeOverlappingIntervals((int, int)[] intervals)
+        {
+            Array.Sort(intervals, (a, b) => a.Item1.CompareTo(b.Item1));
+            List<(int, int)> mergedIntervals = new List<(int, int)>();
+
+
+            for (int i = 0; i < intervals.Length; i++)
+            {
+
+                int start = intervals[i].Item1;
+                int end = intervals[i].Item2;
+
+                if (mergedIntervals.Count > 0 && mergedIntervals[mergedIntervals.Count - 1].Item2 >= end)
+                {
+                    continue;
+                }
+                for (int j = i + 1; j < intervals.Length; j++)
+                {
+                    if (intervals[j].Item1 <= end)
+                        end = Math.Max(end, intervals[j].Item2);
+                }
+                mergedIntervals.Add((start, end));
+            }
+
+            Console.WriteLine("List of merged intervals: ");
+            foreach (var item in mergedIntervals)
+            {
+                Console.WriteLine(item);
+            }
+
+            return mergedIntervals;
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,9 +10,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine(IntervalsDraft.SumIntervals([(4, 8), (9, 10), (15, 21)]));
-        Console.WriteLine(IntervalsDraft.SumIntervals([(1, 5), (10, 20), (1, 6), (16, 19), (5, 11)]));
-        Console.WriteLine(IntervalsDraft.SumIntervals([(1, 5), (1, 6), (5, 11), (10, 20), (16, 19)]));
+        (int, int)[] arr = [(1, 5), (10, 20), (1, 6), (16, 19), (5, 11)];
+        (int, int)[] arr1 = [(7, 8), (1, 5), (2, 4), (4, 6)];
+        Console.WriteLine(Intervals.SumIntervals(arr));
+        Console.WriteLine(Intervals.SumIntervals(arr1));
         
     }
 
